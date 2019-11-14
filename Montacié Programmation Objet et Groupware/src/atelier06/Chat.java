b97b3455aa6c4dcedc7a179c8080ab068a4a3c38
tyratron <tyratron@D219-Tyratron.pedagogie.paris-sorbonne.fr>
2019-11-14 11:06:47 +0100

    Correction Chat

---
+++ Montacié Programmation Objet et Groupware/src/atelier06/Chat.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package atelier06;
+
+/**
+ * @author tyratron
+ *
+ */
+public class Chat extends Felin implements Domesticable {
+
+	private String nom;
+	/**
+	 * @param type
+	 */
+	public Chat(String nom) {
+		super("chats");
+		domestiquer(nom);
+	}
+	
+	/** présentation des caractéristiques du chat */
+	public void presente() {
+		super.presente();  // appel de la méthode de la sur-classe
+		System.out.println("et je m'appelle " + nom());
+	}
+
+	@Override
+	public void domestiquer(String nom) {
+		domestique = true;
+		this.nom = nom;
+	}
+
+	@Override
+	public String nom() {
+		// TODO Auto-generated method stub
+		return this.nom;
+	}
+	
+	/** cri du chat */
+	public void crie() {
+		System.out.println("et je miaule");
+
+	}
+
+}
