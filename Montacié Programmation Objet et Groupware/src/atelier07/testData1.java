7cf335edf40f5e12ba7bd5103de86ab8f4a9ff69
tyratron <tyratron@D219-Tyratron.pedagogie.paris-sorbonne.fr>
2019-11-21 10:41:29 +0100

    Correction de l'exercice 2

---
+++ Montacié Programmation Objet et Groupware/src/atelier07/testData1.java
@@ -0,0 +1,38 @@
+package atelier07;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.SimpleTimeZone;
+import java.util.TimeZone;
+
+public class testData1 {
+
+	public static void main(String[] args) {
+		// get the supported ids for GMT+01:00 (Heure d'Hiver en France)
+		String[] ids = TimeZone.getAvailableIDs(1 * 60 * 60 * 1000);
+		// if no ids were returned, something is wrong. get out.
+		if (ids.length == 0)
+			System.exit(0);
+
+		// create a Heure d'Hiver en France time zone
+		SimpleTimeZone pdt = new SimpleTimeZone(1 * 60 * 60 * 1000, ids[0]);
+		Calendar calendar = new GregorianCalendar(pdt);
+		Date trialTime = new Date();
+		calendar.setTime(trialTime);
+		
+		int jour = calendar.get(Calendar.DAY_OF_MONTH);
+		int mois = calendar.get(Calendar.MONTH) + 1;
+		int annee =  calendar.get(Calendar.YEAR);
+		int heure = calendar.get(Calendar.HOUR);
+		int minute = calendar.get(Calendar.MINUTE);
+		int seconde = calendar.get(Calendar.SECOND);
+		
+		System.out.print(jour+"/"+mois+"/"+annee + " ");
+		System.out.print(heure+":"+minute+":"+seconde);
+		
+		
+
+	}
+
+}
