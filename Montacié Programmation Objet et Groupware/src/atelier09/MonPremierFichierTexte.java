9a069352c89cd3e429a165577e34e71c99a2b079
tyratron <tyratron@D219-Tyratron.pedagogie.paris-sorbonne.fr>
2019-12-12 10:10:45 +0100

    A completer

--- Montacié Programmation Objet et Groupware/src/atelier09/MonPremierFichierTexte.java
+++ Montacié Programmation Objet et Groupware/src/atelier09/MonPremierFichierTexte.java
@@ -9,15 +9,21 @@ public class MonPremierFichierTexte {
 
 	public static void main(String[] args) {
 		String noms[] = {"Montacié","Lejeune","Devillers"};
+		String nfich = "src/atelier09/Nom.txt";
 		
 		try {
-			PrintWriter pr = new PrintWriter(new File("src/atelier09/Nom.txt"));
+			PrintWriter pr = new PrintWriter(new File(nfich));
 			for (int i = 0; i < noms.length;i++) 
 					pr.println(noms[i]);
 		pr.close();
+		System.out.println(verifier(nfich, noms));
 		} 
 		catch (IOException e) { e.printStackTrace(); }
-
+	}
+	
+	public static boolean verifier(String nfich, String[] noms) {
+		return true;
+		
 	}
 
 }
