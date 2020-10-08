c1f1ca3153d129bd8a68dffbc00b51d02b4f2c90
tyratron <tyratron@D219-Tyratron.pedagogie.paris-sorbonne.fr>
2020-10-08 11:45:27 +0200

    MONTACIE

--- Montacié Programmation Objet et Groupware/src/atelier01/MaPremiereConsole.java
+++ Montacié Programmation Objet et Groupware/src/atelier01/MaPremiereConsole.java
@@ -4,15 +4,12 @@ import util.Console;
 
 public class MaPremiereConsole {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		Console c = new Console();
-		c.println("Claude Montaciï¿½");
-
+		c.println("Claude");
+		c.println("Montacié");
+		
 	}
 
 }
