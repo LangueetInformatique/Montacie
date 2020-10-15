185f84fcccb6a5739361c1a69d2bf52c81185839
tyratron <tyratron@D219-Tyratron.pedagogie.paris-sorbonne.fr>
2020-10-15 10:53:54 +0200

    conditionnel3

---
+++ Montacié Programmation Objet et Groupware/src/atelier04/conditionnel3.java
@@ -0,0 +1,24 @@
+package atelier04;
+
+public class conditionnel3 {
+
+	public static void main(String[] args) {
+
+		int p1 = 10, p2 = -1, p3 = 9, p4 = 4, p5 = 8;
+
+		mediane(p1, p2, p3);
+		mediane(p1, p3, p2);
+		mediane(p2, p1, p3);
+		mediane(p2, p3, p1);
+		mediane(p3, p1, p2);
+		mediane(p3, p2, p1);
+	}
+
+	public static int mediane(int p1, int p2, int p3) {
+		int p4 = 0;
+		System.out.print(p4);
+
+		return p4;
+	}
+
+}
