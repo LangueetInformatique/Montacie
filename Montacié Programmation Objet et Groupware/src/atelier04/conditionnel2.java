dcf829e5020ac95b34238dbd204a57893ddaa7f7
tyratron <tyratron@D219-Tyratron.pedagogie.paris-sorbonne.fr>
2020-10-15 10:46:06 +0200

    correction conditionnel2

--- Montacié Programmation Objet et Groupware/src/atelier04/conditionnel2.java
+++ Montacié Programmation Objet et Groupware/src/atelier04/conditionnel2.java
@@ -3,8 +3,32 @@ package atelier04;
 public class conditionnel2 {
 
 	public static void main(String[] args) {
-		int p1 = 10, p2 = 9, p3 = -1, p4 = 4, p5 = 8;
+		int p1 = 10, p2 = -1, p3 = 9, p4 = 4, p5 = 8;
 
+		minimum(p1,p2,p3);
+		minimum(p1,p3,p2);
+		minimum(p2,p1,p3);
+		minimum(p2,p3,p1);
+		minimum(p3,p1,p2);
+		minimum(p3,p2,p1);
 	}
 
+public static int minimum(int p1, int p2, int p3)
+{
+	int p4;
+
+	if (p1 > p2) {
+		if (p2 > p3)
+			p4 = p3;
+		else p4 = p2;
+	}
+	else {
+		if (p1 > p3)
+			p4 = p3;
+		else p4 = p1;
+	}
+	
+	System.out.print(p4);
+	
+	return p4;
 }
