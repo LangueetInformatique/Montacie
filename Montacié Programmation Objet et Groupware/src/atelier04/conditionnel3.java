a8b45582214b1a56fe75a242c2356c5a7805f451
tyratron <tyratron@D219-Tyratron.pedagogie.paris-sorbonne.fr>
2020-10-15 11:10:25 +0200

    correction conditionnel3

--- Montacié Programmation Objet et Groupware/src/atelier04/conditionnel3.java
+++ Montacié Programmation Objet et Groupware/src/atelier04/conditionnel3.java
@@ -15,7 +15,28 @@ public class conditionnel3 {
 	}
 
 	public static int mediane(int p1, int p2, int p3) {
-		int p4 = 0;
+		int p4;
+
+		if (p1 > p2) {
+			if (p3 > p1)
+				p4 = p1;
+			else {
+				if (p3 > p2)
+					p4 = p3;
+				else
+					p4 = p2;
+			}
+		} else {
+			if (p3 > p2)
+				p4 = p2;
+			else {
+				if (p3 > p1)
+					p4 = p3;
+				else
+					p4 = p1;
+			}
+		}
+
 		System.out.print(p4);
 
 		return p4;
