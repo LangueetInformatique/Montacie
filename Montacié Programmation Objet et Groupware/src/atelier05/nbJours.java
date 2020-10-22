a4da63dbb9ef336a5f53910fbe9ab537e065495b
tyratron <tyratron@D219-Tyratron.pedagogie.paris-sorbonne.fr>
2020-10-22 10:36:57 +0200

    correction atelier4 partie 1

--- Montacié Programmation Objet et Groupware/src/atelier05/nbJours.java
+++ Montacié Programmation Objet et Groupware/src/atelier05/nbJours.java
@@ -5,8 +5,17 @@ import cours04.Date;
 public class nbJours {
 
 	public static void main(String[] args) {
-		Date df = new Date(14,7,2021);
-		
+		Date fete = new Date(14, 7, 2021);
+		Date aujourdhui = new Date(22, 10, 2020);
+
+		int nb = 0;
+
+		while (aujourdhui.CompareTo(fete) == true) {
+			aujourdhui.Incrementer();
+			nb++;
+		}
+
+		System.out.println(nb);
 
 	}
 
