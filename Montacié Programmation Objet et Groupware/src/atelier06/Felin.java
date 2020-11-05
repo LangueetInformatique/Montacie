b1bc3bafab6c815c2285017562a27000d231c167
Montacie <Claude.Montacie@Paris-Sorbonne.fr>
2020-11-05 10:24:30 +0100

    Correction du felin

--- Montacié Programmation Objet et Groupware/src/atelier06/Felin.java
+++ Montacié Programmation Objet et Groupware/src/atelier06/Felin.java
@@ -9,16 +9,19 @@ public class Felin extends Animal {
 	 * le constructeur de Felins fait appel au constructeur de la sur-classe Animal
 	 */
 	public Felin(String type) { // les felins ont 4 pattes
-		
-	}					
+		super(type, 4); // appel du construction de la super-classe
+	}					// obligatoire en cas de constructeur non-vide
 
 	/** presentation des caracteristiques du felin */
 	public void presente() {
-		
+		super.presente();  // appel de la methode de la sur-classe
 		String etat = (domestique) ? "domestique" : "sauvage";
 		System.out.println("je suis vraiment un animal " + etat);
 	}
 
 	/** cri du felin */
+	public void crie() {
+		System.out.println("et je rugis");
 
+	}
 }
