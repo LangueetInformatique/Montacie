afcc7415777a1ed0f88df938ae3c15e3071e1249
Montacie <Claude.Montacie@Paris-Sorbonne.fr>
2020-11-05 11:51:55 +0100

    Correction de Chat et testChat

--- Montacié Programmation Objet et Groupware/src/atelier06/Chat.java
+++ Montacié Programmation Objet et Groupware/src/atelier06/Chat.java
@@ -1,22 +1,45 @@
+/**
+ * 
+ */
 package atelier06;
 
+/**
+ * @author tyratron
+ *
+ */
 public class Chat extends Felin implements Domesticable {
 
-	public Chat(String type) {
-		super(type);
-		// TODO Stub du constructeur généré automatiquement
+	private String nom;
+	/**
+	 * @param type
+	 */
+	public Chat(String nom) {
+		super("chats");
+		domestiquer(nom);
+	}
+	
+	/** presentation des caracteristiques du chat */
+	public void presente() {
+		super.presente();  // appel de la m�thode de la sur-classe
+		System.out.println("et je m'appelle " + nom());
 	}
 
 	@Override
 	public void domestiquer(String nom) {
-		// TODO Stub de la méthode généré automatiquement
-
+		domestique = true;
+		this.nom = nom;
 	}
 
 	@Override
 	public String nom() {
-		// TODO Stub de la méthode généré automatiquement
-		return null;
+		// TODO Auto-generated method stub
+		return this.nom;
+	}
+	
+	/** cri du chat */
+	public void crie() {
+		System.out.println("et je miaule");
+
 	}
 
 }
---
+++ Montacié Programmation Objet et Groupware/src/atelier06/testChat.java
@@ -0,0 +1,12 @@
+package atelier06;
+
+public class testChat {
+
+	public static void main(String[] args) {
+		Chat c = new Chat("Filou");
+		c.presente();
+		c.crie();
+
+	}
+
+}
