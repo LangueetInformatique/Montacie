7bb7c6ddd6011632beaff7f6a40a1af21f4e9cec
Montacie <Claude.Montacie@Paris-Sorbonne.fr>
2020-11-19 10:50:53 +0100

    TroisDes

---
+++ Montacié Programmation Objet et Groupware/src/atelier08/TroisDes.java
@@ -0,0 +1,17 @@
+package atelier08;
+
+/** classe enveloppe du resultat du jet de 3 des
+ * 
+ * @author tyratron
+ *
+ */
+public class TroisDes {
+	int d1_, d2_, d3_;
+
+	public TroisDes(int d1, int d2, int d3) {
+		d1_ = d1;
+		d2_ = d2;
+		d3_ = d3;
+	}
+
+}
