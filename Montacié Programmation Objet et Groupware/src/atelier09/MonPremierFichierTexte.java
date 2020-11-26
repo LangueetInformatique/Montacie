2dbff67dbac193b2e40051a0b181a5b5009b99e4
Montacie <Claude.Montacie@Paris-Sorbonne.fr>
2020-11-26 09:58:09 +0100

    corrigé

--- Montacié Programmation Objet et Groupware/src/atelier09/MonPremierFichierTexte.java
+++ Montacié Programmation Objet et Groupware/src/atelier09/MonPremierFichierTexte.java
@@ -1,29 +1,35 @@
 package atelier09;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.BufferedReader;
-import java.io.FileReader;
+
 import util.Keyboard;
 
 public class MonPremierFichierTexte {
+	/*
+	Exercice 1 : Créer la classe exécutable MonPremierFichierTexte 
+	qui écrit dans le fichier texte « Nom.txt » les noms des membres du groupe. 
+	*/
 
-	public static void main(String[] args) {
-		String noms[] = { "Montacie", "Eyharabide", "Devillers" };
+	public static void main (String[] args) {
+		String ligne;
 		String nfich = "src/atelier09/Nom.txt";
-
 		try {
-			PrintWriter pr = new PrintWriter(new File(nfich));
-			for (int i = 0; i < noms.length; i++)
-				pr.println(noms[i]);
+			File f = new File(nfich);
+			
+			PrintWriter pr = new PrintWriter(f);
+			pr.println("Morgann");
+			pr.println("Julien");
+			pr.println("Mustapha");
+			pr.println("Chenwangli");
+			pr.println("Ana");
+			pr.println("Joyce");
+			
 			pr.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+			System.out.println("Fin d'écriture fichier");
+		
 	}
-
-	
-
+		catch (IOException e) {e.printStackTrace(); }
+}
 }
