7d684ebcf7b43812fd236943be20e477aee5dc93
alexane.jouglar <alexane.jouglar@localhost>
2020-12-10 10:26:40 +0100

    Proposition

--- Montacié Programmation Objet et Groupware/src/atelier11/Partie2.java
+++ Montacié Programmation Objet et Groupware/src/atelier11/Partie2.java
@@ -2,7 +2,11 @@ package atelier11;
 
 import java.awt.GridLayout;
 
-public class Partie2 {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Partie2 extends JFrame{
 	
 	/*
 	 * Les classes de type Layout font partie du package java.awt. 
@@ -25,5 +29,19 @@ public class Partie2 {
 	 * 
 	 */
 	GridLayout salle219 = new GridLayout(5, 4);
-
+	
+	public Partie2() {
+	JFrame frame = new JFrame();
+    frame.setVisible(true);
+    JPanel panel = new JPanel();
+    frame.setContentPane(panel);
+    panel.setLayout(salle219);
+    
+    panel.add(new JButton("Julien"));
+    panel.add(new JButton("Ana"));
+    panel.add(new JButton("Chengwanli"));
+    panel.add(new JButton("Mustapha"));
+    panel.add(new JButton("Alexane"));
+    panel.add(new JButton("Morgann"));
+	}
 }
